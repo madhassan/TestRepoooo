87755ad208739c9e4256858c080e7811018b7192
Ahmed Ghanafer <ahmed.ghanafer@gmail.com>
2018-04-12 14:53:52 +0200

    zsds

---
+++ Garage/Garage/GarageClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class GarageClass<T> :​ ​IEnumerable<T>​ where T : Vehicle
+    {
+        private T[] vehicles;
+        int cap, count;
+
+        public GarageClass(int capacity)
+        {
+            cap = capacity;
+            count = 0;
+            vehicles = new T[capacity];
+        }
+        
+        public void Leave(T input)
+        {
+            if (cap > count)
+            {
+                vehicles[count++] = input;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return vehicles[i];
+            }
+        }
+
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Garage/Garage/UI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codealong180410
+{
+    public class UI
+    {
+        public void MainMenu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter number for choice");
+                Console.WriteLine("1) Example");
+                Console.WriteLine("0) Quit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Example();
+                        break;
+                    case "0":
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input");
+                        break;
+                }
+                Console.Write("\n<PRESS ANY BUTTON TO CONTINUE>");
+                Console.ReadKey();
+            }
+        }
+
+        public void Example()
+        {
+            var g  = new AnimalShelter<Animal>(10);
+            theShelter.Leave(new Animal(57, "zafa", "horse", 4, true));
+            theShelter.Leave(new Animal(100, "Hati", "Elephant", 4, true));
+            theShelter.Leave(new Animal(2, "Jessica", "Dog", 3, true));
+            theShelter.Leave(new Animal(3, "Herr Nilsson", "Monkey", 2, true));
+            theShelter.Leave(new Animal(50, "Trump", "Orange", 2, false));
+
+            var filteredShelter = theShelter.Where(x => x.NrOfLegs > 2 || x.Species == "Orange").OrderBy(x => x.IsTame).OrderBy(x => x.Age).OrderBy(x => x.Name)
+            foreach (Animal a in filteredShelter)
+            {
+                Console.WriteLine($"{a.Name} is {a.Age} years old and has {a.NrOfLegs} legs, is a {a.Species} and is a tame {a.IsTame}");
+            }
+
+        }
+    }
+
+}
